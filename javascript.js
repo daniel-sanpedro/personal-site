e93c864ae2bb514022ee4207fe954a7910b106f6
daniel-sanpedro <dpsanpedro@yahoo.com>
2024-02-05 20:46:45 -0800

    <submitted Javascript>

--- javascript.js
+++ javascript.js
@@ -1,16 +1,1 @@
-console.log("Hello World");
-
-describe("myFunction", function() {
-    it("should add the class 'dark-mode' to the <body> element when the function is called", function() {
-        // Arrange
-        var element = document.body;
-        var originalClassList = element.classList;
-        var expectedClassList = new Set(["dark-mode"]);
-
-        // Act
-        myFunction();
-
-        // Assert
-        expect(element.classList).toEqual(expectedClassList);
-    });
-});
+console.log("Hello World")
