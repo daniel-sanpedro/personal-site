d6f5999959a915c586c81027f27d86ffeda6d40f
daniel-sanpedro <dpsanpedro@yahoo.com>
2024-02-17 12:40:41 -0800

    <css minecraft>

--- javascript.js
+++ javascript.js
@@ -3,3 +3,12 @@ function submitForm() {
   window.location("success.html");
 }
 
+const form = document.querySelector('#form')
+const submitButton = document.querySelector('#submit')
+
+form.addEventListener('submit', (e) => {
+  submitButton.disabled = true
+  e.preventDefault()
+  window.location.href = window.location.origin + '/success.html'
+})
+
